e7fb249f8378c7167f5df9651b53f416e8a6751c
Alessandro Pighetti <s4359171@studenti.unige.it>
2022-05-19 18:22:10 +0200

    thursday push pigostation

--- highway_env/envs/dm_env.py
+++ highway_env/envs/dm_env.py
@@ -13,7 +13,7 @@ from highway_env.vehicle.kinematics import Vehicle
 from highway_env.vehicle.objects import LaneIndex
 
 # START_SEC = 120
-COL_REWARDS = [-0.1, -0.5, -1, -2.5] # ordini di grandezza differenti
+COL_REWARDS = [-0.05, -0.5, -1, -3] # ordini di grandezza differenti
 
 class DecisionMakingEnv(AbstractEnv):
     """
@@ -43,14 +43,14 @@ class DecisionMakingEnv(AbstractEnv):
             "controlled_vehicles": 1,
             "initial_lane_id": None,
             "duration": 120,  # [s]
-            "ego_spacing": 2,
+            "ego_spacing": 0.5,
             "vehicles_density": 0.7,
             # "collision_reward": -3,            // COLLISION IS DIVIDED # The reward received when colliding with a vehicle.
-            "not_in_right_lane_reward": -0.004,  # The reward received when driving on the right-most lanes, linearly mapped to
+            "not_in_right_lane_reward": -0.08,  # The reward received when driving on the right-most lanes, linearly mapped to
                                                  # zero for other lanes.
-            "distance_to_tv_reward": -0.07,      # -0.015 // non basta come incentivo alla velocità
+            "distance_to_tv_reward": -0.05,      # -0.015 // non basta come incentivo alla velocità
             # "decision_change_reward": -0.25,   // NOT IMPLEMENTED YET
-            "distance_reward": 0.05,
+            "distance_reward": 0.03,
             # "high_speed_reward": 0.001,        # The reward received when driving at full speed, linearly mapped to zero for
                                                  # lower speeds according to config["reward_speed_range"].
             # "lane_change_reward": -0.005,      # The reward received at each lane change action.
