33da16e0f4624074cd4a348876f4c32d3966263c
Alessandro Pighetti <s4359171@studenti.unige.it>
2022-05-26 12:27:44 +0200

    testing env upload

--- highway_env/envs/dm_env.py
+++ highway_env/envs/dm_env.py
@@ -41,19 +41,19 @@ class DecisionMakingEnv(AbstractEnv):
             },
             "screen_width": 1250,
             "lanes_count": 2,
-            "vehicles_count": 1, # curriculum learning su lanes e npc-vehicles
+            "vehicles_count": 5, # curriculum learning su lanes e npc-vehicles
             "controlled_vehicles": 1,
             "initial_lane_id": 1,
             "duration": 120,  # [s]
             "ego_spacing": 1,
             "vehicles_density": 0.7,
             "collision_reward": -1,            # The reward received when colliding with a vehicle.
-            # "not_in_right_lane_reward": -0.08,  # The reward received when driving on the right-most lanes, linearly mapped to
+            "not_in_right_lane_reward": -0.4,  # The reward received when driving on the right-most lanes, linearly mapped to
             #                                      # zero for other lanes.
-            # "distance_to_tv_reward": -0.01,      # -0.015 // non basta come incentivo alla velocità
+            "distance_to_tv_reward": -0.5,      # -0.015 // non basta come incentivo alla velocità
             # "decision_change_reward": -0.25,   // NOT IMPLEMENTED YET
             # "distance_reward": 0.08,
-            "high_speed_reward": 0.3,        # The reward received when driving at full speed, linearly mapped to zero for
+            "high_speed_reward": 0.5,        # The reward received when driving at full speed, linearly mapped to zero for
                                                  # lower speeds according to config["reward_speed_range"].
             # "lane_change_reward": -0.005,      # The reward received at each lane change action.
             "reward_speed_range": [30, 36],
@@ -65,9 +65,9 @@ class DecisionMakingEnv(AbstractEnv):
         w = self.vehicles_distribution()
         self._create_road()
         self._create_vehicles(w)
-        f = open(r'C:\Users\luka-\Desktop\ACC_data.csv', 'a')
-        f.write("ego_speed,front_vehicle_speed,throttle,distance,gap,counter" + "\n")
-        f.close()
+        # f = open(r'C:\Users\luka-\Desktop\ACC_data.csv', 'a')
+        # f.write("ego_speed,front_vehicle_speed,throttle,distance,gap,counter" + "\n")
+        # f.close()
 
     def _create_road(self) -> None:
         """Create a road composed of straight adjacent lanes."""
@@ -116,8 +116,8 @@ class DecisionMakingEnv(AbstractEnv):
             for i in range(others):
                 aux = random.choices(range(0,self.config['lanes_count']), weights = vehicle_distribution, k=1)[0]
                 # vehicle = other_vehicles_type.create_random(self.road, lane_id=self.config["npc_initial_lane_id"], spacing=1 / self.config["vehicles_density"])
-                vehicle = other_vehicles_type.create_random(self.road, speed = self.get_npc_speed(aux,range(0,self.config['lanes_count'])),\
-                    lane_id = 1, spacing=1 / self.config["vehicles_density"]) #edit NPC
+                vehicle = other_vehicles_type.create_random(self.road, speed = 24,\
+                    lane_id = 1, spacing=8 / self.config["vehicles_density"]) #edit NPC
                 vehicle.randomize_behavior()
                 self.road.vehicles.append(vehicle)
 
@@ -192,7 +192,7 @@ class DecisionMakingEnv(AbstractEnv):
         # else:
         #     not_in_rl = 0
 
-        # speed_diff = utils.lmap((36 - self.vehicle.speed), [0,36] , [0,1])
+        speed_diff = utils.lmap((36 - self.vehicle.speed), [0,36] , [0,1])
 
         # duration_diff = utils.lmap((self.config['duration'] - self.steps), [self.config['duration'],0], [0,1])
         # self.TOTAL_SPACE += abs(self.vehicle.speed*(self.steps - self.LAST_STEPS))
@@ -224,15 +224,15 @@ class DecisionMakingEnv(AbstractEnv):
         # COL_REWARDS[collision_index]
 
         reward = self.config["collision_reward"] * self.vehicle.crashed \
+            + self.config["distance_to_tv_reward"] * speed_diff \
             + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
 
             # + self.config["distance_reward"] * km_travelled
 
-            # + self.config["distance_to_tv_reward"] * speed_diff \
             # + self.config["not_in_right_lane_reward"] * (1 - (lane / max(len(neighbours) - 1, 1))) \
  
         reward = utils.lmap(reward,
-                          [self.config["collision_reward"],
+                          [self.config["collision_reward"] + self.config["distance_to_tv_reward"],
                            self.config["high_speed_reward"]],
                           [0, 1])
 
